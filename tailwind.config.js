a98a5d245bfbb11d0fe648887240d1dccab4e78b
Francisco Navarro <francisconavarroflores@gmail.com>
2024-03-02 02:16:39 -0300

    instalado tailwind css para produccion

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./descargas.html",
+    "./experiencia_entel.html",
+    "./home.html",
+    "./inspiracion.html",
+    "./projects.html",
+    "./resume.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
