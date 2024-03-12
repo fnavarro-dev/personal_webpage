d80e04e29538c337511c12bd373d20fd23ab5d6a
Francisco Navarro <francisconavarroflores@gmail.com>
2024-03-12 02:53:34 -0300

    update vista mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
     "./inspiracion.html",
     "./projects.html",
     "./resume.html",
+    "./posts/experiencia-entel.html",
+    "./posts/proyecto-alcohol-&-multiplayer.html",
+    "./posts/proyecto-sitio-web-personal.html",
+    "./posts/proyecto-tocata-con-los-cabros.html",
+    "./posts/sitio-web-personal.html",
+    "./posts/sitio-web-personal copy.html",
+    "./*.html",
+    "./posts/**/*.html",
+
   ],
   theme: {
     extend: {},
