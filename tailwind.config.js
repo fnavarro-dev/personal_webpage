65dee05fe79de22c05f292309c1463b7c9e053f2
Francisco Navarro <francisconavarroflores@gmail.com>
2024-03-26 03:49:20 -0300

    eliminado exceso de imagenes gifs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: false,
   content: [
-    // "./descargas.html",
-    // "./experiencia_entel.html",
-    // "./home.html",
-    // "./inspiracion.html",
-    // "./projects.html",
-    // "./resume.html",
-    // "./posts/experiencia-entel.html",
-    // "./posts/proyecto-alcohol-&-multiplayer.html",
-    // "./posts/proyecto-sitio-web-personal.html",
-    // "./posts/proyecto-tocata-con-los-cabros.html",
-    // "./posts/sitio-web-personal.html",
-    // "./posts/sitio-web-personal copy.html",
-    // "./*.html",
-    "./navbar.html",
+    "./descargas.html",
+    "./experiencia_entel.html",
+    "./home.html",
+    "./inspiracion.html",
+    "./projects.html",
+    "./resume.html",
+    "./posts/experiencia-entel.html",
+    "./posts/proyecto-alcohol-&-multiplayer.html",
+    "./posts/proyecto-sitio-web-personal.html",
+    "./posts/proyecto-tocata-con-los-cabros.html",
+    "./posts/sitio-web-personal.html",
+    "./posts/sitio-web-personal copy.html",
+    "./*.html",
+    //"./navbar.html",
     
 
   ],
