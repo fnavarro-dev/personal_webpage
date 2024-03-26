3dd1fc1e2fa7c99ee4c440ceb3b2165c7271cee8
Francisco Navarro <francisconavarroflores@gmail.com>
2024-03-26 02:24:56 -0300

    componente navbar tailwind css con mods para no chocar con bootstrap en seccion blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    enabled: true,
   content: [
-    "./descargas.html",
-    "./experiencia_entel.html",
-    "./home.html",
-    "./inspiracion.html",
-    "./projects.html",
-    "./resume.html",
-    "./posts/experiencia-entel.html",
-    "./posts/proyecto-alcohol-&-multiplayer.html",
-    "./posts/proyecto-sitio-web-personal.html",
-    "./posts/proyecto-tocata-con-los-cabros.html",
-    "./posts/sitio-web-personal.html",
-    "./posts/sitio-web-personal copy.html",
-    "./*.html",
-    "./posts/**/*.html",
+    // "./descargas.html",
+    // "./experiencia_entel.html",
+    // "./home.html",
+    // "./inspiracion.html",
+    // "./projects.html",
+    // "./resume.html",
+    // "./posts/experiencia-entel.html",
+    // "./posts/proyecto-alcohol-&-multiplayer.html",
+    // "./posts/proyecto-sitio-web-personal.html",
+    // "./posts/proyecto-tocata-con-los-cabros.html",
+    // "./posts/sitio-web-personal.html",
+    // "./posts/sitio-web-personal copy.html",
+    // "./*.html",
+    "./navbar.html",
+    
 
   ],
+},
   theme: {
     extend: {
       screens: {
@@ -26,4 +30,5 @@ module.exports = {
   },
   plugins: [],
 }
+
 
